6e41df3c8b44b32dba42eb545decaa3bd1115d1b
f1bu920 <2377478740@qq.com>
2020-11-25 21:04:52 +0800

    初始化提交

---
+++ go.mod
@@ -0,0 +1,35 @@
+module github.com/EDDYCJY/go-gin-example
+
+go 1.15
+
+require (
+	github.com/astaxie/beego v1.12.3
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-ini/ini v1.62.0
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
+	github.com/ugorji/go v1.2.0 // indirect
+	github.com/unknwon/com v1.0.1
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9 // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
+
+replace (
+	github.com/EDDYCJY/go-gin-example/conf => ../go-application/go-gin-example/pkg/conf
+	github.com/EDDYCJY/go-gin-example/middleware => ../go-application/go-gin-example/middleware
+	github.com/EDDYCJY/go-gin-example/models => ../go-application/go-gin-example/models
+	github.com/EDDYCJY/go-gin-example/pkg => ../go-application/go-gin-example/pkg/setting
+	github.com/EDDYCJY/go-gin-example/pkg/e => ../go-application/go-gin-example/pkg/e
+	github.com/EDDYCJY/go-gin-example/pkg/setting => ../go-application/go-gin-example/pkg/setting
+	github.com/EDDYCJY/go-gin-example/pkg/util => ../go-application/go-gin-example/pkg/util
+	github.com/EDDYCJY/go-gin-example/routers => ../go-application/go-gin-example/routers
+//github.com/EDDYCJY/go-gin-example/routers/api/v1 => ../go-application/go-gin-example/routers/api/v1
+)
---
+++ routers/api/v1/tag.go
@@ -0,0 +1,130 @@
+package v1
+
+import (
+	"github.com/EDDYCJY/go-gin-example/models"
+	"github.com/EDDYCJY/go-gin-example/pkg/e"
+	"github.com/EDDYCJY/go-gin-example/pkg/setting"
+	"github.com/EDDYCJY/go-gin-example/pkg/util"
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+	"net/http"
+)
+
+//获取多个文章标签
+func GetTags(c *gin.Context) {
+	name := c.Query("name")
+
+	maps := make(map[string]interface{})
+	data := make(map[string]interface{})
+	if name != "" {
+		maps["name"] = name
+	}
+	state := -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		maps["state"] = state
+	}
+	code := e.SUCCESS
+	data["lists"] = models.GetTags(util.GetPage(c), setting.PageSize, maps)
+	data["total"] = models.GetTagTotal(maps)
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
+}
+
+//新增文章标签
+func AddTag(c *gin.Context) {
+	name := c.Query("name")
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	createdBy := c.Query("created_by")
+
+	valid := validation.Validation{}
+	valid.Required(name, "name").Message("Tag名称不能为空")
+	valid.MaxSize(name, 100, "name").Message("Tag名称最长100字符")
+	valid.Required(createdBy, "created_by").Message("Tag创建人不能为空")
+	valid.MaxSize(createdBy, 100, "created_by").Message("Tag创建人名称最长100字符")
+	valid.Range(state, 0, 1, "state").Message("状态只允许为0和1")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		if !models.ExistTagByName(name) {
+			code = e.SUCCESS
+			models.AddTag(name, state, createdBy)
+		} else {
+			code = e.ERROR_EXIST_TAG
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
+}
+
+//修改文章标签
+func EditTag(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+	name := c.Query("name")
+	modifiedBy := c.Query("modified_by")
+
+	valid := validation.Validation{}
+	var state int = -1
+	if args := c.Query("state"); args != "" {
+		state = com.StrTo(args).MustInt()
+		valid.Range(state, 0, 1, "state").Message("Tag状态只允许0和1")
+	}
+	valid.Required(name, "name").Message("Tag名称不能为空")
+	valid.Required(modifiedBy, "modified_by").Message("修改人不能为空")
+	valid.MaxSize(name, 100, "name").Message("Tag名称最长为100字符")
+	valid.MaxSize(modifiedBy, 100, "modified_by").Message("修改人名称最长为100")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		code = e.SUCCESS
+		if models.ExistTagById(id) {
+			data := make(map[string]interface{})
+			data["modified_by"] = modifiedBy
+			if name != "" {
+				data["name"] = name
+			}
+			if state != -1 {
+				data["state"] = state
+			}
+			models.EditTag(id, data)
+		} else {
+			code = e.ERROR_NOT_EXIST_TAG
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
+}
+
+//删除文章标签
+func DeleteTag(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("id必须大于0")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		code = e.SUCCESS
+		if models.ExistTagById(id) {
+			models.DeleteTag(id)
+		} else {
+			code = e.ERROR_NOT_EXIST_TAG
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
+}
